d0ca1fc469da7c9d0d015ad09cbdb8a3924abf5a
justin <jemurphy@vcu.edu>
2016-02-21 11:43:17 -0500

    import file, format columns, output head of dataframe on web page.

--- data_logs.py
+++ data_logs.py
@@ -1,57 +1,59 @@
+#!/usr/env
 import pandas as pd
 import re
-def retrieve_conf_file():
-    conf_file = open('./Configuration/log.conf')
-    return conf_file
-def retrieve_log_location(conf_file):
-    for line in conf_file:
-        if re.match('source_file', line):
-            return line.split('\'', 1)[1].split('\'',1)[0]
-def retrieve_file_type(conf_file):
-    for line in conf_file:
-        if re.match('source_file_type', line):
-            return line.split('\'', 1)[1].split('\'',1)[0]
- #   return re.search('source_file',conf_file)
-def retrieve_data(log_location, file_type):
-    if file_type == 'csv':
-        log_data = pd.read_csv(log_location)
-        return log_data
-    else:
-        return 'invalid data type'
-def retrieve_column_headings(conf_file):
-    column_keys=['standardized_time','human_readable_time','source_ip','destination_ip','login_account',
-                 'log_source_device','log_message']
-    column_headings={}
-    for key in column_keys:
+class data_logs:
+    def retrieve_conf_file():
+        conf_file = open('./Configuration/log.conf')
+        return conf_file
+    def retrieve_log_location(conf_file):
         for line in conf_file:
-            if re.match(key, line):
-                column_headings[key]=line.split('\'', 1)[1].split('\'',1)[0]
-                break
-    for line in conf_file:
-        if re.match('custom[01-99]', line):
-            column_headings[line.split('=', 1)[0]]=line.split('\'', 1)[1].split('\'',1)[0]
-    return column_headings
-def format_data(log_data, column_headings):
-    print(log_data.columns)
-    print(column_headings)
-    formatted_log=pd.DataFrame()
-    for key in column_headings:
-        if log_data[column_headings[key]].any():
-            formatted_log.loc[:,column_headings[key]]=log_data[column_headings[key]]
+            if re.match('source_file', line):
+                return line.split('\'', 1)[1].split('\'',1)[0]
+    def retrieve_file_type(conf_file):
+        for line in conf_file:
+            if re.match('source_file_type', line):
+                return line.split('\'', 1)[1].split('\'',1)[0]
+     #   return re.search('source_file',conf_file)
+    def retrieve_data(log_location, file_type):
+        if file_type == 'csv':
+            log_data = pd.read_csv(log_location)
+            return log_data
         else:
-            print(column_headings[key])
-    return formatted_log
-#def analyze_data():
+            return 'invalid data type'
+    def retrieve_column_headings(conf_file):
+        column_keys=['standardized_time','human_readable_time','source_ip','destination_ip','login_account',
+                     'log_source_device','log_message']
+        column_headings={}
+        for key in column_keys:
+            for line in conf_file:
+                if re.match(key, line):
+                    column_headings[key]=line.split('\'', 1)[1].split('\'',1)[0]
+                    break
+        for line in conf_file:
+            if re.match('custom[01-99]', line):
+                column_headings[line.split('=', 1)[0]]=line.split('\'', 1)[1].split('\'',1)[0]
+        return column_headings
+    def format_data(log_data, column_headings):
+        print(log_data.columns)
+        print(column_headings)
+        formatted_log=pd.DataFrame()
+        for key in column_headings:
+            if log_data[column_headings[key]].any():
+                formatted_log.loc[:,column_headings[key]]=log_data[column_headings[key]]
+            else:
+                print(column_headings[key])
+        return formatted_log
+    #def analyze_data():
 
 def main():
-    conf_file=retrieve_conf_file()
-    log_location=retrieve_log_location(conf_file)
-    file_type=retrieve_file_type(conf_file)
-    master_log=retrieve_data(log_location,file_type)
+    conf_file=data_logs.retrieve_conf_file()
+    log_location=data_logs.retrieve_log_location(conf_file)
+    file_type=data_logs.retrieve_file_type(conf_file)
+    master_log=data_logs.retrieve_data(log_location,file_type)
     print(len(master_log))
-    column_headings=retrieve_column_headings(conf_file)
+    column_headings=data_logs.retrieve_column_headings(conf_file)
     print(column_headings.values())
-    formatted_log=format_data(master_log,column_headings)
+    formatted_log=data_logs.format_data(master_log,column_headings)
     print(formatted_log.head())
 #    print(master_log.head())
 if __name__ == "__main__":
--- driver.py
+++ driver.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from data_logs import data_logs
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    conf_file=data_logs.retrieve_conf_file()
+    log_location=data_logs.retrieve_log_location(conf_file)
+    file_type=data_logs.retrieve_file_type(conf_file)
+    master_log=data_logs.retrieve_data(log_location,file_type)
+    return render_template('dashboard.html', name=master_log.head().to_html())
+    #return str(master_log.head())
 
 @app.route('/asdf/')
 def hello_world1():
