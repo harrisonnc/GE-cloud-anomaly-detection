d7c90d7e39a805f6f67481ba4ffe24ae9a79727a
justin <jemurphy@vcu.edu>
2016-02-21 12:24:54 -0500

    import file, format columns, output head of dataframe on web page.

--- driver.py
+++ driver.py
@@ -8,7 +8,9 @@ def hello_world():
     log_location=data_logs.retrieve_log_location(conf_file)
     file_type=data_logs.retrieve_file_type(conf_file)
     master_log=data_logs.retrieve_data(log_location,file_type)
-    return render_template('dashboard.html', name=master_log.head().to_html())
+    column_headings=data_logs.retrieve_column_headings(conf_file)
+    formatted_log=data_logs.format_data(master_log,column_headings)
+    return render_template('dashboard.html', name=formatted_log.head().to_html())
     #return str(master_log.head())
 
 @app.route('/asdf/')
