540f4019164f9aa9eb95e34e4a05b95a3688bed9
Nick Harrison <n1ck.harrison64@gmail.com>
2016-03-29 14:54:34 -0400

    added edited graph

--- log_graphs.py
+++ log_graphs.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from flask import Flask, send_file
 app = Flask(__name__)
 from data_logs import data_logs
 
 
 class graphics(data_logs):
+    global graphArrays
+    graphArrays=[]
+
     def totalGraph(self):
         df = data_logs()
         times = df.formatted_log['startTime'].tolist()
@@ -33,13 +37,15 @@ class graphics(data_logs):
             else:
                 pass
 
-        plt.plot(clumptime, clumps)
-        plt.axis('tight')
-        plt.title('Total Logins over Time')
-        plt.ylabel('login attempts')
-        plt.xlabel('time (epoch)')
-        plt.tight_layout()
-        plt.savefig('./static/img/totalGraph.png')
+        graphArrays.append(clumptime)
+        graphArrays.append(clumps)
+        #plt.plot(clumptime, clumps)
+        #plt.axis('tight')
+        #plt.title('Total Logins over Time')
+        #plt.ylabel('login attempts')
+        #plt.xlabel('time (epoch)')
+        #plt.tight_layout()
+        #plt.savefig('./static/img/totalGraph.png')
         #plt.show()
 
 
@@ -52,25 +58,29 @@ class graphics(data_logs):
         eventList.sort()
         failure_value = ['A Kerberos authentication ticket (TGT) was rejected', 'Web Service Login Failed']
         times.sort()
-        failcount = 1
+        failcount = 0
         fails = []
         failtimes = []
 
 
-        for count1 in range(0, len(df.formatted_log)):
+        for count1 in range(1, len(df.formatted_log)):
             if eventList[count1] == failure_value[0]:
                 fails += [failcount]
                 failcount += 1
                 failtimes += [times[count1]]
-
-        plt.clf()
-        plt.plot(failtimes, fails)
-        plt.axis('tight')
-        plt.title('Failed Logins over Time')
-        plt.ylabel('failed attempts')
-        plt.xlabel('time (epoch)')
-        plt.tight_layout()
-        plt.savefig('./static/img/failGraph.png')
+            else:
+                fails+=[failcount]
+                failtimes+= [times[count1]]
+        graphArrays.append(failtimes)
+        graphArrays.append(fails)
+        #plt.clf()
+        #plt.plot(failtimes, fails)
+        #plt.axis('tight')
+        #plt.title('Failed Logins over Time')
+        #plt.ylabel('failed attempts')
+        #plt.xlabel('time (epoch)')
+        #plt.tight_layout()
+        #plt.savefig('./static/img/failGraph.png')
         #plt.show()
 
     def successGraph(self):
@@ -83,38 +93,44 @@ class graphics(data_logs):
         successCount = 1
         passes = []
         passtimes = []
+        fails=0
         passcount=0
         time=times[0]
         passes.append(0)
         passtimes.append(time)
 
+
+        #while failure_value[0] in eventList:
+         #   eventList.remove(failure_value[0])
+
         for count in range(0, len(df.formatted_log)):
             if eventList[count] == failure_value[0]:
-                pass
+                fails=fails+1
+            if times[count]-time>=1200000:
+                time=times[count]
+                passcount=count-passcount
+                passtimes+=[time]
+                passes+=[passcount-fails]
+                successCount=successCount+1
+            elif count==len(eventList)-1:
+                time=times[count]
+                passcount=count-passcount
+                passtimes+=[time]
+                passes+=[passcount-fails]
+                successCount=successCount+1
             else:
-                if times[count]-time>=1200000:
-                    time=times[count]
-                    passcount=count-passcount
-                    passtimes+=[time]
-                    passes+=[passcount]
-                    successCount=successCount+1
-                elif count==len(df.formatted_log)-1:
-                    time=times[count]
-                    passcount=count-passcount
-                    passtimes+=[time]
-                    passes+=[passcount]
-                    successCount=successCount+1
-                else:
-                    pass
+                pass
 
-        plt.plot(passtimes, passes)
-        plt.axis('tight')
+        graphArrays.append(passtimes)
+        graphArrays.append(passes)
+        #plt.plot(passtimes, passes)
+        #plt.axis('tight')
         # plt.axis([times[0],times[len(df.formatted_log)-1],0,logins[len(logins)-1]])
-        plt.title('Successful Logins over Time')
-        plt.ylabel('successful attempts')
-        plt.xlabel('time (epoch)')
-        plt.tight_layout()
-        plt.savefig('./static/img/successGraph.png')
+        #plt.title('Successful Logins over Time')
+        #plt.ylabel('successful attempts')
+        #plt.xlabel('time (epoch)')
+        #plt.tight_layout()
+        #plt.savefig('./static/img/successGraph.png')
         #plt.show()
 
     def main(self):
@@ -122,9 +138,26 @@ class graphics(data_logs):
         graph.totalGraph()
         graph.successGraph()
         graph.failGraph()
+        plt.plot(graphArrays[0],graphArrays[1],'b',graphArrays[2],graphArrays[3],'g',graphArrays[4],graphArrays[5],'r')
+        blue_patch = mpatches.Patch(color='blue', label='Total Logins')
+        green_patch = mpatches.Patch(color='green', label='Successful Logins')
+        red_patch = mpatches.Patch(color='red', label='Failed Logins')
+        plt.legend(handles=[blue_patch,green_patch,red_patch],loc=2,shadow=True)
+        plt.title('Login Rates over Time')
+        plt.axis('tight')
+        plt.savefig('./static/img/combinedGraph.png')
 
 if __name__ == '__main__':
     graph = graphics()
     graph.totalGraph()
     graph.successGraph()
     graph.failGraph()
+    plt.plot(graphArrays[0],graphArrays[1],'b',graphArrays[2],graphArrays[3],'g',graphArrays[4],graphArrays[5],'r')
+    blue_patch = mpatches.Patch(color='blue', label='Total Logins')
+    green_patch = mpatches.Patch(color='green', label='Successful Logins')
+    red_patch = mpatches.Patch(color='red', label='Failed Logins')
+    plt.legend(handles=[blue_patch,green_patch,red_patch],loc=2,shadow=True)
+    plt.title('Login Rates over Time')
+    plt.axis('tight')
+    plt.savefig('./static/img/combinedGraph.png')
+    #plt.show()
